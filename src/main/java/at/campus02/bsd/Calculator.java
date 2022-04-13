b9f120595aca367359715474cfa46e880218cc06
Monika Popic <s50826@edu.campus02.at>
2022-04-13 13:12:28 +0200

    changed3

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,2 +1,25 @@
-package at.campus02.bsd;public class Calculator {
+package at.campus02.bsd;
+
+public class Calculator {
+
+    public double add(double number1, double number2){
+        double value = number1+number2;
+        return value;
+    }
+
+    public double minus(double number1, double number2){
+        double value = number1-number2;
+        return value;
+    }
+
+    public double divide(double number1, double number2){
+        double value = number1/number2;
+        return value;
+    }
+
+    public double multiply(double number1, double number2){
+        double value = number1*number2;
+        return value;
+    }
+
 }
--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,2 +1,13 @@
-package at.campus02.bsd;public class Main {
+package at.campus02.bsd;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(5,2));
+        System.out.println(calculator.minus(5,2));
+        System.out.println(calculator.divide(5,2));
+        System.out.println(calculator.multiply(5,2));
+    }
 }
