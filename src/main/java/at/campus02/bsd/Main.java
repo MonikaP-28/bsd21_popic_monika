4f9359194ab882973b28a2e099a1df4c8f41d82b
Monika Popic <s50826@edu.campus02.at>
2022-05-20 17:57:46 +0200

    error logger

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,15 +1,27 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
+//
+//        System.out.println(calculator.add(5,2));
+//        System.out.println(calculator.minus(5,2));
+//        System.out.println(calculator.divide(5,2));
+//        System.out.println(calculator.multiply(5,2));
+//        System.out.println(calculator.faculty(5));
 
-        System.out.println(calculator.add(5,2));
-        System.out.println(calculator.minus(5,2));
-        System.out.println(calculator.divide(5,2));
-        System.out.println(calculator.multiply(5,2));
-        System.out.println(calculator.faculty(5));
+        // only error gets printed (info doesn't)
+        logger.error("Is is an error logger.");
+        logger.info("It is an info logger.");
+
+
 
     }
 }
